a1f692e7ca9ea188205186436616105360293dd4
Tuan Nguyen <83622137+mrwhosethesus@users.noreply.github.com>
2021-12-07 10:03:02 -0600

    I AM NOT GRR
    Kira and Cryptofyre is >:) UwU

---
+++ benisnotsus.py
@@ -0,0 +1,13 @@
+#BEN IS NOT SUS
+print("This is a joke that we have for fun on Twitter. Please don't take anything seriously lol")
+whoisnotsus = input("Who is very sus? ")
+notsus = "Ben"
+if whoisnotsus != notsus:
+    print("Correct! " + whoisnotsus + " is very sus")
+            
+elif whoisnotsus == notsus:
+        print("No u. You are so sus for saying I am sus. Kira and Cryptofyre is for making bensussy.py and benissussy on Go")
+        whoisnotsus = input("Now, who is sus? ")
+        if whoisnotsus != notsus:
+            print("Correct! " + whoisnotsus + " is very sus.")
+            quit()
